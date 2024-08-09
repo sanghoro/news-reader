10ba5ba8fc8eb97ed040ddbfa15613c236b800c7
Seong Kang <seong.h.kang2@gmail.com>
2024-08-08 19:17:16 -0500

    fetched data successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Mainpage from './mainpage/Mainpage'
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=4d20c82c3c37412ea4e03c58d5f5516c')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setArticles(data)
+    })
+  }, [])
+
   return (
     <div className="App">
-        <Mainpage />
+        <Mainpage articles={articles}/>
     </div>
   );
 }
--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Detailed from '../detailed/Detailed'
 
-const Mainpage = () => {
+const Mainpage = ({ articles }) => {
+
+    const articleInformation = articles.map(article => {
+        return (
+            <Detailed
+                id={article.source.id}
+                title={article.title}
+                image={article.urlToImage}
+                author={article.author}
+                content={article.content}
+                description={article.description}
+                publishedAt={article.publishedAt}
+            />
+        )
+    })
+
+
     return (
-        <div>Mainpage</div>
+        <div>
+            {articleInformation}
+        </div>
     )
 }
 
