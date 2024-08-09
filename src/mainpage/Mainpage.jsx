46b36074362301edbfd31d5d8523df8b1cf4ceca
Seong Kang <seong.h.kang2@gmail.com>
2024-08-08 20:08:35 -0500

    styled mainpage

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
-import Detailed from '../detailed/Detailed'
+import "./Mainpage.css"
 
-const Mainpage = ({ articles }) => {
-
-    const articleInformation = articles.map(article => {
-        return (
-            <Detailed
-                id={article.source.id}
-                title={article.title}
-                image={article.urlToImage}
-                author={article.author}
-                content={article.content}
-                description={article.description}
-                publishedAt={article.publishedAt}
-            />
-        )
-    })
-
-
+const Mainpage = ({ articles, viewDetails }) => {
     return (
-        <div>
-            {articleInformation}
+        <div className="news-container">
+            <h1 className='logo'>News Reader</h1>
+
+            <div className="article-container">
+                {articles.map(article => (
+                    <div key={article.url} className="card">
+                        <h2 className="title">{article.title}</h2>
+                        <img src={article.urlToImage} className='article-image' />
+                        <p className="description">{article.description}</p>
+                    </div>
+                ))}
+            </div>
+
         </div>
     )
 }
