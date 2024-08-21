bf7137079c4e8f6621593af74d375a9491dbb2db
Seong <seong.h.kang2@gmail.com>
2024-08-21 09:19:32 -0500

    Update Mainpage.jsx

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom';
-import "./Mainpage.css"
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Mainpage.css";
 
 const Mainpage = ({ articles }) => {
-    const navigate = useNavigate();
     const [searchTerm, setSearchTerm] = useState("");
 
-    const handleClick = (url) => {
-        navigate(`/details/${url}`);
-    };
-
-    const filteredArticles = articles.filter(article => article.title.toLowerCase().includes(searchTerm.toLowerCase()))
-
+    const filteredArticles = articles.filter(article =>
+        article.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <div className="news-container">
@@ -19,16 +15,26 @@ const Mainpage = ({ articles }) => {
                 <h1 className='logo'>The Turing Times</h1>
             </Link>
             <div className='search-section'>
-                <input className="search-bar" type="search" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                <input
+                    className="search-bar"
+                    type="search"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
             </div>
 
             <div className="article-container">
                 {filteredArticles.map((article, index) => (
-                    <div key={index} className="card" onClick={() => handleClick(index)}>
-                        <h2 className="title" >{article.title}</h2>
-                        <img src={article.urlToImage} className='article-image' />
+                    <Link
+                        key={index}
+                        to={`/details/${index}`}
+                        className="card"
+                    >
+                        <h2 className="title">{article.title}</h2>
+                        <img src={article.urlToImage} className='article-image' alt={article.title} />
                         <p className="description">{article.description}</p>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
